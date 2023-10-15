d098c09fcb9c535b52abdedb4f931c99e78cc548
Madhav Kanna <madhavkanna3141@gmail.com>
2023-10-15 08:56:28 -0400

    changes

--- routes/classify-api.js
+++ routes/classify-api.js
@@ -2,25 +2,16 @@ const express = require('express');
 const tf = require('@tensorflow/tfjs-node');
 const classifyApiRouter = express.Router();
 const predictionMap = {
-    0: 'Not Waste', 1: 'Organic Waste', 2: 'Recyclable Waste', 3: 'Electronic Waste', 4: 'Regular Waste'
+    0: 'Organic Waste', 1: 'Recyclable'
 }
 classifyApiRouter.post("/classify", [], async function (req, res) {
-    const model = await tf.loadLayersModel('../model.json');
-    const inputElement = req.params.data;
-    const tensor = tf.browser.fromPixels(inputElement)
-        .resizeNearestNeighbor([224, 224])
-        .toFloat()
-        .expandDims();
+    const modelInfo = await tf.node.getMetaGraphsFromSavedModel('../model.json');
+    console.log(modelInfo);
+    // const model = await tf.loadLayersModel('../model.json');
 
-    // Use ResNet50 preprocessing (mean subtraction)
-    const offset = tf.scalar(127.5);
-    const processedTensor = tensor.sub(offset).div(offset);
-
-    // Make prediction
-    const predictions = model.predict(processedTensor);
-    res.send({
-        class: predictionMap[predictions]
-    })
+    // res.send({
+    //     class: predictionMap[predictions]
+    // })
 });
 
 
