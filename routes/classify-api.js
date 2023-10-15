b66e4f1757e44ae48dccba1d3d4ac3c99100b355
Madhav Kanna <madhavkanna3141@gmail.com>
2023-10-15 09:03:06 -0400

    changes

--- routes/classify-api.js
+++ routes/classify-api.js
@@ -5,14 +5,15 @@ const predictionMap = {
     0: 'Organic Waste', 1: 'Recyclable'
 }
 classifyApiRouter.post("/classify", [], async function (req, res) {
-    const modelInfo = await tf.node.getMetaGraphsFromSavedModel('../model.json');
-    console.log(modelInfo);
+    // const modelInfo = await tf.node.getMetaGraphsFromSavedModel('../model.json');
+    // console.log(modelInfo);
     // const model = await tf.loadLayersModel('../model.json');
 
     // res.send({
     //     class: predictionMap[predictions]
     // })
-    res.send("in progress please wait");
+    const response = { data: 'Organic Waste' };
+    res.send(JSON.stringify(response));
 });
 
 
